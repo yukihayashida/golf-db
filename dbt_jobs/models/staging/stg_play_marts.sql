8941fee920cd58eaaa4a294b28630d17c8f3d834
Yuki Hayashida <yuki_hayashida@r.recruit.co.jp>
2022-09-18 17:19:01 +0900

    dbt run clear

---
+++ dbt_jobs/models/staging/stg_play_marts.sql
@@ -0,0 +1,12 @@
+SELECT
+s.*
+, p.course_name
+, p.play_date
+
+FROM
+{{ source('src_source_gss', 'gss_play') }} AS p
+INNER JOIN
+{{ source('src_source_gss', 'gss_hole_score') }} AS s
+
+ON
+p.playid = s.playid
