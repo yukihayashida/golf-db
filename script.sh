f3b7523c58524a912e929afdc19590264032aa24
Yuki Hayashida <yuki_hayashida@r.recruit.co.jp>
2022-09-24 18:18:31 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,22 @@
 
 echo "script start."
 
-echo `ls`
+dbt deps --profiles-dir .
+if [[ $? == 1 ]]; then
+ echo 'dbt deps is faild'
+ exit 1
+fi
 
-dbt run --target ${_DBT_TARGET} --profiles-dir .
+dbt debug --target ${_DBT_TARGET} --profiles-dir .
+if [[ $? == 1 ]]; then
+ echo 'dbt debug is faild'
+ exit 1
+fi
+
+dbt build --target ${_DBT_TARGET} --profiles-dir .
+if [[ $? == 1 ]]; then
+ echo 'dbt build is faild'
+ exit 1
+fi
 
 echo "script end."
