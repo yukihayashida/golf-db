ac02d116f45b37b8771014cb678a9f9309ab4437
Yuki Hayashida <yuki_hayashida@r.recruit.co.jp>
2024-03-31 17:21:18 +0900

    add elementary

--- script.sh
+++ script.sh
@@ -20,4 +20,10 @@ if [[ $? == 1 ]]; then
  exit 1
 fi
 
+edr monitor --slack-token ${SLACK_TOKEN_ELEMENTARY} --slack-channel-name dbt-alerts
+if [[ $? == 1 ]]; then
+ echo '[script.sh] elementary monitor is faild'
+ exit 1
+fi
+
 echo "[script.sh] script end."
